156c6f372237cbbaf3a1acd654e254a9391c7615
JoeyBream <jjsb3@cam.ac.uk>
2023-11-27 16:00:15 +0000

    Replacing B.append(A[i][j] with B.append(j)

--- Challenge: Flatten/flatten.py
+++ Challenge: Flatten/flatten.py
@@ -2,11 +2,11 @@
 '''flatten([[1, 2], [3, 4]]) should return [1, 2, 3, 4]'''
 def flatten(A):
     B = []
-    for i in range(len(A)):
-        for j in range(len(A[i])):
-            B.append(A[i][j])
+    for i in A:
+        for j in i:
+            B.append(j)
     return B
 
 ### Test ###
 flatten([1, 2, 3, 4])
-flatten([[1, 2][3,4]])
+flatten([[1, 2],[3,4]])
