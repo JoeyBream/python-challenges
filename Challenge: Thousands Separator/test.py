779d6a194c3244e6c4e77d16a29c0dba7ec01194
Joey Bream <94135932+JoeyBream@users.noreply.github.com>
2023-11-28 21:55:01 +0000

    Improving test cases

--- Challenge: Thousands Separator/test.py
+++ Challenge: Thousands Separator/test.py
@@ -2,8 +2,9 @@ from format_number import *
 import random
 
 for i in range (10):
-    x = random.uniform(20, 60)
-    decimals = random.randrange(0,10)
+    x = random.uniform(20, 600)
+    decimals = random.randrange(1,10)
     formatted_result = format_number(x, decimals)
-    assert round(x,decimals) == float(formatted_result), f"Error: Original Number {x} not equal to Formatted Result {formatted_result}"
+    # Fix the assertion - currently you can't convert the result back into a float 
+    assert round(x,decimals) == float(formatted_result), f"Error: Original Number {x} not equal to Formatted Result {formatted_result}. This resulted from using {decimals} decimals."
     print(f"Original Number: {x}, Decimals: {decimals}, Formatted Result: {formatted_result}")
