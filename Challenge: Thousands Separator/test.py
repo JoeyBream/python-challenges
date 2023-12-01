c9fbe8875551bf03a875aac57996fa8f610314d6
Joey Bream <94135932+JoeyBream@users.noreply.github.com>
2023-12-01 13:10:33 +0000

    Removing unecessary text

--- Challenge: Thousands Separator/test.py
+++ Challenge: Thousands Separator/test.py
@@ -5,5 +5,4 @@ for i in range (10):
     x = random.uniform(20, 600)
     decimals = random.randrange(1,10)
     formatted_result = format_number(x, decimals)
-    #TODO: #1  Fix the assertion - currently you can't convert the result of the above line back into a float 
     print(f"Original Number: {x}, Decimals: {decimals}, Formatted Result: {formatted_result}")
