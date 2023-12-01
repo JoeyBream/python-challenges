d0b249532ca34c8f5950a148a2f6368ed5c5e34c
Joey Bream <94135932+JoeyBream@users.noreply.github.com>
2023-12-01 13:12:17 +0000

    Returning test case

--- Challenge: Thousands Separator/test.py
+++ Challenge: Thousands Separator/test.py
@@ -5,4 +5,5 @@ for i in range (10):
     x = random.uniform(20, 600)
     decimals = random.randrange(1,10)
     formatted_result = format_number(x, decimals)
+    assert round(x,decimals) == float(formatted_result), f"Error: Original Number {x} not equal to Formatted Result {formatted_result}. This resulted from using {decimals} decimals."
     print(f"Original Number: {x}, Decimals: {decimals}, Formatted Result: {formatted_result}")
