3a4c8d65c5c14e3c6c82c6b3519388e59844183c
Joey Bream <94135932+JoeyBream@users.noreply.github.com>
2023-12-08 15:54:38 +0000

    Working to simplify this program

--- Other_Challenges:/consecutive_zeros.py
+++ Other_Challenges:/consecutive_zeros.py
@@ -1,4 +1,5 @@
 '''Takes a string, returns the number of consecutive zeros'''
+import time
 def consecutive_zeros(string):
     full_max = []
     current_max = 0
@@ -6,10 +7,13 @@ def consecutive_zeros(string):
         if letter == "0":
             current_max += 1
             full_max.append(current_max)
-        if letter != "0":
-            current_max = 0
+        else:
+            current_max = 0 # Reset the maximum count found. Unecessary unless this is the first time 
     return max(full_max, default=0)
     
+
+start = time.time_ns()
+
 print(consecutive_zeros("asdaf"))
 print(consecutive_zeros("10001"))
 print(consecutive_zeros("101"))
@@ -17,3 +21,6 @@ print(consecutive_zeros("11"))
 print(consecutive_zeros("0000"))
 print(consecutive_zeros("000000-00-0-0"))
 print(consecutive_zeros(""))
+
+end = time.time_ns()
+print(end - start)
