71b759c0c233dd424b02d877032c41eade8c8d67
Joey Bream <94135932+JoeyBream@users.noreply.github.com>
2023-12-08 15:37:35 +0000

    Adding consecutive_zeros

---
+++ Other_Challenges:/consecutive_zeros.py
@@ -0,0 +1,19 @@
+'''Takes a string, returns the number of consecutive zeros'''
+def consecutive_zeros(string):
+    full_max = []
+    current_max = 0
+    for letter in string:
+        if letter == "0":
+            current_max += 1
+            full_max.append(current_max)
+        if letter != "0":
+            current_max = 0
+    return max(full_max, default=0)
+    
+print(consecutive_zeros("asdaf"))
+print(consecutive_zeros("10001"))
+print(consecutive_zeros("101"))
+print(consecutive_zeros("11"))
+print(consecutive_zeros("0000"))
+print(consecutive_zeros("000000-00-0-0"))
+print(consecutive_zeros(""))
