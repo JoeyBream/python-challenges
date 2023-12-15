bf03bc6e037b978e96f2bfe778a1b4dcf091ffbc
Joey Bream <94135932+JoeyBream@users.noreply.github.com>
2023-12-15 12:02:05 +0000

    Updating all_equal

--- Other_Challenges:/17-all_equal.py
+++ Other_Challenges:/17-all_equal.py
@@ -1,1 +1,10 @@
+"Define a function named all_equal that takes a list and checks whether all elements in the list are the same.
 
+For example, calling all_equal([1, 1, 1]) should return True."
+
+def all_equal(list):
+    for i in range(len(list)):
+        if list[0] != list[i]:
+            return False
+            break
+    return True
